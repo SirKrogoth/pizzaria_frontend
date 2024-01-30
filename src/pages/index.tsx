412d4fa0eb90e0bf652a061f3c6d31caac693f97
João Rafael Menezes <menezes.jrafael@gmail.com>
2024-01-30 02:32:18 -0300

    aha

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,27 @@ import Image from 'next/image';
 import { Input } from '../components/ui/Input/index';
 import { Button } from '../components/ui/Button';
 import Link from 'next/link';
+import { useContext, FormEvent, useState } from 'react';
+import { AuthContext } from '../contexts/AuthContext';
 
 export default function Home() {
+  const { signIn } = useContext(AuthContext);
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  async function handleLogin(event: FormEvent){
+    event.preventDefault();
+
+    let data = {
+      email,
+      password
+    }
+
+    await signIn(data);
+  }
+
   return (
     <>
       <Head>
@@ -17,14 +36,18 @@ export default function Home() {
           <Image src={logoImg} alt="Logo Sujeito Pizzaria" />
 
           <div className={styles.login}>
-            <form>
+            <form onSubmit={handleLogin}>
               <Input
               placeholder='Digite seu email'
               type='text'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               />
               <Input
               placeholder='Digite sua senha'
               type='password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               />
 
               <Button type="submit" loading={false}>
